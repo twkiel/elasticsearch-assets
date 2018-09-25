b077533f98609fef6c842cf9dad01a127f24799b
Jared Noble <jsnoble1@gmail.com>
2018-09-25 06:27:11 -0700

    formatted package.json and methods of client

--- spec/mock_client.js
+++ spec/mock_client.js
@@ -15,18 +15,6 @@ class MockClient {
         this.cluster = {};
         this.deepRecursiveResponseCount = false;
 
-        this.search = async () => {
-            const { sequence } = this;
-            if (sequence.length > 0) {
-                return sequence.shift();
-            }
-            const total = this.deepRecursiveResponseCount || 0;
-            return {
-                _shards: { failed: 0 },
-                hits: { total }
-            };
-        };
-
         this.indices.getSettings = async () => {
             const window = 10000;
             return {
@@ -44,21 +32,34 @@ class MockClient {
             const defaultVersion = '2.1.1';
             return { nodes: { versions: [defaultVersion] } };
         };
-
-        this.setSequenceData = (data) => {
-            this.sequence = data.map(
-                obj => ({
-                    _shards: { failed: 0 },
-                    hits: {
-                        total: obj.count !== undefined ? obj.count : 100,
-                        hits: [{ _source: obj }]
-                    }
-                })
-            );
-        };
-
-        this.bulk = async data => data;
     }
+
+    async search() {
+        const { sequence } = this;
+        if (sequence.length > 0) {
+            return sequence.shift();
+        }
+        const total = this.deepRecursiveResponseCount || 0;
+        return {
+            _shards: { failed: 0 },
+            hits: { total }
+        };
+    }
+
+    setSequenceData(data) {
+        this.sequence = data.map(
+            obj => ({
+                _shards: { failed: 0 },
+                hits: {
+                    total: obj.count !== undefined ? obj.count : 100,
+                    hits: [{ _source: obj }]
+                }
+            })
+        );
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    async bulk(data) { return data; }
 }
 
 module.exports = MockClient;
