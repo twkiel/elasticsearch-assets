e45e9f5047c268fb1f0ebef4053c6df3439a0ac3
peterdemartini <thepeterdemartini@gmail.com>
2019-03-08 14:19:03 -0700

    fix lint

--- asset/id_reader/id_slicer.js
+++ asset/id_reader/id_slicer.js
@@ -1,9 +1,11 @@
 'use strict';
 
+/* eslint-disable no-useless-escape, no-restricted-syntax */
+
 const _ = require('lodash');
 const parseError = require('@terascope/error-parser');
 const { retryModule } = require('../utils');
-/* eslint-disable no-useless-escape no-restricted-syntax */
+
 const base64url = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
     'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
     'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\-', '_'];
