eefd99c089479ff6d8b4f89077134145a7d37328
Jared Noble <jsnoble1@gmail.com>
2019-06-27 07:20:35 -0700

    elasticsearch state storage

---
+++ asset/elasticsearch-state-storage/interfaces.js
@@ -0,0 +1,8 @@
+
+// TODO: keeping for future use of typescript
+
+// import { APIConfig } from '@terascope/job-components';
+// import { ESStateStorageConfig } from '@terascope/teraslice-state-storage';
+
+// export interface ESStorageConfig extends ESStateStorageConfig, APIConfig {
+// }
