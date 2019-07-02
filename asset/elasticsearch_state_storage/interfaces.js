b492680f1084e77e50939e0ac83da5355aba491f
Jared Noble <jsnoble1@gmail.com>
2019-07-02 08:42:06 -0700

    remove interfaces

--- asset/elasticsearch_state_storage/interfaces.js
+++
@@ -1,8 +0,0 @@
-
-// TODO: keeping for future use of typescript
-
-// import { APIConfig } from '@terascope/job-components';
-// import { ESStateStorageConfig } from '@terascope/teraslice-state-storage';
-
-// export interface ESStorageConfig extends ESStateStorageConfig, APIConfig {
-// }
